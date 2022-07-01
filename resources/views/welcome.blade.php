62f9a3c5fe3108f3e8d28bf4123599b74fd4f0d1
Mohammad484554 <mjibreel48@gmail.com>
2022-07-01 06:52:28 -0700

    production enviroment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        This is Staging Environment - Mohammad Jibreel
+        This is prodction Environment - Mohammad Jibreel
         </div>
     </body>
 </html>
